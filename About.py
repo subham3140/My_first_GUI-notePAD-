df4650dd94a3fa187ca08cb8600576ca83fc77da
subham3140 <59496508+subham3140@users.noreply.github.com>
2020-11-16 09:56:58 +0530

    Update About.py

--- About.py
+++ About.py
@@ -43,10 +43,6 @@ def about():
     t.insert(END,"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n\n")
     t.insert(END,"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
 
-    # activate_f = Frame(root,width=580,height=20,bg="#706d6d")
-    # activate_f.pack(fill=BOTH,expand=True)
-    # activate_f.place(x=6.3,y=580)
-
     activate= Button(root,text="Activate notePAD",width=50,command=Register.register)
     activate.place(x=120,y=580)
 
